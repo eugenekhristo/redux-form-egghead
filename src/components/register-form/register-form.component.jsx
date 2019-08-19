e1dbaed7152e28c19243a4c33b7b481d84ed551a
Eugene Khristo <marteau9292@gmail.com>
2019-08-19 11:35:42 +0300

    Another way of validating (with validate functions)

--- src/components/register-form/register-form.component.jsx
+++ src/components/register-form/register-form.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { CustomInput, CustomSelect } from './fields/index';
-import { validate } from './validation';
+import { required, minLength, maxLength } from './validation';
 
 class RegisterForm extends Component {
   render() {
@@ -14,18 +14,21 @@ class RegisterForm extends Component {
           type="text"
           component={CustomInput}
           label="Firstname"
+          validate={[required]}
         />
         <Field
           name="surname"
           type="text"
           component={CustomInput}
           label="Surname"
+          validate={[required]}
         />
         <Field
           name="username"
           type="text"
           component={CustomInput}
           label="Username"
+          validate={[required, minLength, maxLength]}
         />
         <Field
           name="preference"
@@ -44,4 +47,4 @@ class RegisterForm extends Component {
   }
 }
 
-export default reduxForm({ form: 'register', validate })(RegisterForm);
+export default reduxForm({ form: 'register' })(RegisterForm);
--- src/components/register-form/validation/index.js
+++ src/components/register-form/validation/index.js
@@ -1,22 +1,28 @@
-export const validate = values => {
-  const errors = {};
-  const { firstname, surname, username } = values;
+export const required = value => (value ? undefined : 'This field is required');
+export const minLength = value =>
+  value.length <= 3 ? 'This field must be more than 3 chars' : undefined;
+export const maxLength = value =>
+  value.length >= 10 ? 'This field must be less than 10 chars' : undefined;
 
-  if (!firstname) {
-    errors.firstname = 'Firstname is required';
-  }
+// export const validate = values => {
+//   const errors = {};
+//   const { firstname, surname, username } = values;
 
-  if (!surname) {
-    errors.surname = 'Surname is required';
-  }
+//   if (!firstname) {
+//     errors.firstname = 'Firstname is required';
+//   }
 
-  if (!username) {
-    errors.username = 'Username is required';
-  } else if (username.length <= 3) {
-    errors.username = 'Username must be more than 3 chars';
-  } else if (username.length >= 10) {
-    errors.username = 'Username must be less than 10 chars';
-  }
+//   if (!surname) {
+//     errors.surname = 'Surname is required';
+//   }
 
-  return errors;
-};
+//   if (!username) {
+//     errors.username = 'Username is required';
+//   } else if (username.length <= 3) {
+//     errors.username = 'Username must be more than 3 chars';
+//   } else if (username.length >= 10) {
+//     errors.username = 'Username must be less than 10 chars';
+//   }
+
+//   return errors;
+// };
